8abb11b8b805927833a4b4c559a103c7bb003a96
Paulo Luiz Fachini <paulofachini@gmail.com>
2014-06-25 20:22:45 -0300

    Informações do sistema e da equipe.

--- ponto/application/views/home/index.php
+++ ponto/application/views/home/index.php
@@ -6,7 +6,16 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-6">
-                                Teste
+                                    Sistema de Controle de Ponto.<br>
+                                    Trabalho Final Módulo 2 UDESC - Pós-Graduação lato sensu em Engenharia de Software.<br>
+                                    <br>
+                                    Acadêmicos:<br>
+                                    Luiz Fernando Debatin (@ldebatin)<br>
+                                    Marcos Busana (@MarcosBusana)<br>
+                                    Paulo Luiz Fachini (@paulofachini)<br>
+                                    Thiago Fabian Lenzi (@lenzi)<br>
+                                    <br>
+                                    Professor: Nilson Modro (@nilsonmodro)
                                 </div>
                             </div>
                         </div>
