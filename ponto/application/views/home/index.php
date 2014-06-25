b7642c56225beea01c9a36bc27aee718577467ae
Paulo Luiz Fachini <paulofachini@gmail.com>
2014-06-25 20:27:47 -0300

    Informações da equipe.

--- ponto/application/views/home/index.php
+++ ponto/application/views/home/index.php
@@ -4,5 +4,15 @@
                 	<center>
 	                	<img src="<?php echo URL; ?>public/img/sisponto.png" alt="sisponto">
                 	</center>
+                    <br>
+                    Trabalho Final Módulo 2 UDESC - Pós-Graduação lato sensu em Engenharia de Software.<br>
+                    <br>
+                    Acadêmicos:<br>
+                    Luiz Fernando Debatin (@ldebatin)<br>
+                    Marcos Busana (@MarcosBusana)<br>
+                    Paulo Luiz Fachini (@paulofachini)<br>
+                    Thiago Fabian Lenzi (@lenzi)<br>
+                    <br>
+                    Professor: Nilson Modro (@nilsonmodro)
                 </div>
             </div>
